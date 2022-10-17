d386a3490d92aff064b28ba6b42db9332ecef9a5
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-17 16:59:24 +0200

    trying to use querySelectorAll

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -2,50 +2,50 @@
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
 
-const fieldForm = document.getElementById('form');
-const boxes = document.getElementsByClassName('box');
+const container = document.querySelector('.container');
+const button = document.getElementById('generate')
 const difficulties = document.getElementById('difficulties');
-console.log(fieldForm);
+console.log(container);
 
 
 
 
-fieldForm.addEventListener('submit', (event)=>{
-    event.preventDefault();
+button.addEventListener('click', ()=>{
     let gridWidth = Number(difficulties.value);
     let cellsNum = gridWidth * gridWidth;
-    if(!fieldForm.classList.contains('generated')){
-        generateGrid(cellsNum, fieldForm);
+    if(!container.classList.contains('generated')){
+        generateGrid(cellsNum, container);
         generateNumbersWidth(boxes, gridWidth);
     }
     
-    
-});
+})
 
 
 difficulties.addEventListener('input', ()=>{
-    fieldForm.classList.remove('generated');
+    container.classList.remove('generated');
 });
 
 
 
-
+const boxes = document.querySelectorAll('div.box');
 
 
 function generateGrid(cellsNum, domEl){
-    resetGrid();
     for (let i = 1; i <= cellsNum; i++){
         let boxEl = document.createElement('div');
         boxEl.classList.add('box');
         domEl.insertAdjacentElement('afterbegin', boxEl)
     }
     domEl.classList.add('generated');
+    console.log(boxes)
+    
 }
 
 function generateNumbersWidth(domElements, rowNum){
     for(let i = 0; i < domElements.length; i++){
         let number = i;
         let element = domElements[i];
+        console.log(element, i);
         element.innerText = ++number;
         element.style.width =`calc(100%/${rowNum})`
         element.addEventListener('click', ()=>{
@@ -54,6 +54,7 @@ function generateNumbersWidth(domElements, rowNum){
             element.classList.toggle('clicked');
         })
     }
+    
 
 }
 
@@ -62,7 +63,7 @@ function resetGrid() {
     for(let i = boxes.length - 1; i >= 0; i--){
         boxes[i].remove();
     }
-    console.log(boxes);
+   
 }
 
 
