4c22a97d3140bf65b0153791e0a99753b5361057
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-17 13:40:11 +0200

    added method to generate numbers and log the num

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
 
 const fieldForm = document.getElementById('form');
+const boxes = document.getElementsByClassName('box');
 console.log(fieldForm);
 
 
@@ -11,16 +12,29 @@ console.log(fieldForm);
 fieldForm.addEventListener('submit', (event)=>{
     event.preventDefault();
     generateGrid(100, fieldForm);
+    generateNumbers(boxes);
 });
 
 
 
 function generateGrid(cellsNum, domEl){
-    for (let i = 0; i < cellsNum; i++){
+    for (let i = 1; i <= cellsNum; i++){
         let boxEl = document.createElement('div');
         boxEl.classList.add('box');
         domEl.insertAdjacentElement('afterbegin', boxEl)
     }
+}
+
+function generateNumbers(domElements){
+    for(let i = 0; i < domElements.length; i++){
+        let number = i;
+        let element = domElements[i];
+        element.innerText = ++number;
+        element.addEventListener('click', ()=>{
+            console.log(this.innerText);
+        })
+    }
+
 }
 
 
